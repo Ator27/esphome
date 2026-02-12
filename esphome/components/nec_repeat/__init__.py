9a53c8034cd3f2e58f9330b9b8cd0935d6a8eb0e
Ator27 <86977797+Ator27@users.noreply.github.com>
2026-02-12 05:54:12 -0600

    Refactor NECRepeatProtocol and update CONFIG_SCHEMA

--- esphome/components/nec_repeat/__init__.py
+++ esphome/components/nec_repeat/__init__.py
@@ -1,16 +1,29 @@
 import esphome.codegen as cg
 from esphome.components import remote_base
+import esphome.config_validation as cv
 
 CODEOWNERS = ["@Ator27"]
 
-# Create a new namespace for our protocol
+# Namespace for our custom protocol
 nec_repeat_ns = cg.esphome_ns.namespace("nec_repeat")
-NECRepeatProtocol = nec_repeat_ns.class_("NECRepeatProtocol", remote_base.RemoteProtocol)
 
-# This is the name you'll use under remote_receiver: dump:
-CONFIG_SCHEMA = remote_base.validate_protocol({
-    cg.GenerateID(): cg.declare_id(NECRepeatProtocol),
-})
+# C++ class declaration
+NECRepeatProtocol = nec_repeat_ns.class_(
+    "NECRepeatProtocol",
+    remote_base.RemoteProtocol,
+)
+
+# This allows: dump: nec_repeat
+CONFIG_SCHEMA = remote_base.validate_protocol(
+    {
+        cv.GenerateID(): cv.declare_id(NECRepeatProtocol),
+    }
+)
+
+# Tell ESPHome we depend on remote_base
+AUTO_LOAD = ["remote_base"]
 
 async def to_code(config):
-    cg.add(NECRepeatProtocol.new())
+    # Register the protocol instance
+    var = cg.new_Pvariable(config[cg.CONF_ID])
+    cg.add(var)
