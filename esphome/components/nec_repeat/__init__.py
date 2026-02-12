3ea27867dff1beb06687ef70f94e0cfe48ee9225
Ator27 <86977797+Ator27@users.noreply.github.com>
2026-02-12 05:49:00 -0600

    Refactor NEC repeat component to use new protocol

--- esphome/components/nec_repeat/__init__.py
+++ esphome/components/nec_repeat/__init__.py
@@ -1,9 +1,16 @@
-from esphome.components import remote_receiver
-import esphome.config_validation as cv
-from esphome.const import CONF_ID
+import esphome.codegen as cg
+from esphome.components import remote_base
 
 CODEOWNERS = ["@Ator27"]
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(remote_receiver.RemoteReceiver),
+# Create a new namespace for our protocol
+nec_repeat_ns = cg.esphome_ns.namespace("nec_repeat")
+NECRepeatProtocol = nec_repeat_ns.class_("NECRepeatProtocol", remote_base.RemoteProtocol)
+
+# This is the name you'll use under remote_receiver: dump:
+CONFIG_SCHEMA = remote_base.validate_protocol({
+    cg.GenerateID(): cg.declare_id(NECRepeatProtocol),
 })
+
+async def to_code(config):
+    cg.add(NECRepeatProtocol.new())
